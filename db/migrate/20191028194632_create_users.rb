ef4f844f8158f17afc46a8048f100659c0e8d334
Astrid Corena <astrid.corena@gmail.com>
2019-10-28 15:59:52 -0500

    Prueba

---
+++ db/migrate/20191028194632_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :lastname
+      t.integer :phone
+      t.string :email
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
